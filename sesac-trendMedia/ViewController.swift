b8bfa9498714ec585dbe18839c2935d0ec97351c
minseongkim97 <kyrok67@naver.com>
2022-08-04 02:31:33 +0900

    Initial Commit

---
+++ sesac-trendMedia/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  sesac-trendMedia
+//
+//  Created by MIN SEONG KIM on 2022/08/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
